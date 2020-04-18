b24a33f1894481b87309240515895930312d0bf5
CagriBurakAy <cagriburakay@gmail.com>
2020-04-19 02:10:31 +0300

    Initial commit

---
+++ service/src/main/java/com/example/service/doctor/DoctorService.java
@@ -0,0 +1,29 @@
+package com.example.service.doctor;
+
+
+import com.example.dao.doctor.DoctorRepository;
+
+import com.example.mailservice.EmailService;
+import com.example.model.doctor.Doctor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+@Service
+public class DoctorService {
+    @Autowired
+    private DoctorRepository repository;
+    @Autowired
+    private EmailService service;
+    @PostConstruct
+    public void initDoctor(){
+        repository.saveAll(Stream.of(new Doctor(101,"John","Cardiac"),new Doctor(102,"Jack","Eye")).collect(Collectors.toList()));
+    }
+    public List<Doctor> getDoctors(){
+        service.sendEmail();
+        return repository.findAll();
+    }
+}
